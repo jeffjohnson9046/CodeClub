ba258f410f9f065cae5863c9c5512642b0e3ca0a
Jeff Johnson <jeffjohnson9046@hotmail.com>
2012-08-13 00:06:39 -0700

    Added debugging text and HanoiPuzzleTest.cs

---
+++ CodeClub.TowersOfHanoi/test/CodeClub.TowersOfHanoi.UnitTests/HanoiPuzzleTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeClub.TowersOfHanoi.Core.Recursive;
+
+namespace CodeClub.TowersOfHanoi.UnitTests
+{
+    [TestClass]
+    public class HanoiPuzzleTest
+    {
+        [TestMethod]
+        public void HanoiPuzzle_Recursive_Solution_With_Four_Disks()
+        {
+            #region Arrange
+
+            var puzzle = new HanoiPuzzle();
+
+            #endregion
+
+
+            #region Act
+
+            puzzle.Solve(4, "sourcePeg", "destinationPeg", "intermediatePeg");
+
+            #endregion
+
+
+            #region Assert
+
+            #endregion
+        }
+    }
+}
