7f4c77a347e2a6d2858d29d91298b818b87a032a
Jeff Johnson <jeffjohnson9046@hotmail.com>
2012-08-31 07:37:08 -0700

    removed numberOfMoves counter

--- CodeClub.TowersOfHanoi/src/CodeClub.TowersOfHanoi.Core/Recursive/HanoiPuzzle.cs
+++ CodeClub.TowersOfHanoi/src/CodeClub.TowersOfHanoi.Core/Recursive/HanoiPuzzle.cs
@@ -9,25 +9,19 @@ namespace CodeClub.TowersOfHanoi.Core.Recursive
     {
         public void Solve(int numberOfDisks, string source, string destination, string spare)
         {
-            Console.WriteLine("SOLVE:  numberOfDisks: {0}, source: {1}, destination: {2}, spare: {3}", numberOfDisks, source, destination, spare);
-            int numberOfMoves = 0;
-
             if (numberOfDisks > 0)
             {
                 int disk = numberOfDisks - 1;
                 this.Solve(disk, source, spare, destination);
-                this.Move(disk, source, destination, ref numberOfMoves);
-
-                Console.WriteLine("Calling SOLVE again... numberOfDisks: {0}, source: {1}, destination: {2}, intermediate: {3}", disk, spare, destination, source);
+                this.Move(disk, source, destination);
                 this.Solve(disk, spare, destination, source);
             }
         }
 
-        private void Move(int disk, string source, string destination, ref int numberOfMoves)
+        private void Move(int disk, string source, string destination)
         {
             string message = string.Format("Moved disk {2} from {0} tower to {1} tower.", source, destination, disk);
             Console.WriteLine(message);
-            numberOfMoves++;
         }
     }
 }
